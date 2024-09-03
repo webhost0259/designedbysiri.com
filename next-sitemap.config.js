70ed69dc9cdff2b4203af7a1a6b980e1c9f04efb
Sree K <skankara.webhost@gmail.com>
2024-09-02 19:36:20 -0500

    Adding Sitemap.xml and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://designedbysiri.com',
+    generateRobotsTxt: true, // (Optional) Generate robots.txt file
+    sitemapSize: 7000, // (Optional) Max number of URLs per sitemap file
+};
