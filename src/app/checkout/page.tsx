e4de008567c49f56380d5bd7cef649b61b1bd347
Sree K <skankara.webhost@gmail.com>
2025-03-10 22:57:17 +0530

    Fixing redirect url

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -94,7 +94,7 @@ const CheckoutPage = () => {
         merchantTransactionId: transactionId,
         customerId: customer?.customerId || '',
         amount: cart.reduce((total, item) => total + item.price * item.quantity, 0) * 100,
-        redirectUrl: `https://designedbysiri.com/users/payment-status`,
+        redirectUrl: `https://www.designedbysiri.com/users/orders`,
         // redirectUrl: `https://designedbysiri.com/users/payment-status?transactionId=${transactionId}`,
         mobileNumber: customer?.phone || 0,
       });
